eb1bd7b3c271b55eda68f79144a88b3ca4a977ba
spieden <spieden@exaptic.com>
2015-05-09 01:08:27 -0700

    aaand actually commit the source code

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject watership "0.1.0-SNAPSHOT"
+  :description "Consume RabbitMQ queues with core.async channels"
+  :url "https://github.com/spieden/watership"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [com.novemberain/langohr "3.0.1"]])
+
